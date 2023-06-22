54e283be05ef899abc68aa93efc0cd43f5acb9f1
wonchuz <120187423+wonchuz@users.noreply.github.com>
2023-06-23 01:15:22 +0800

    updated errors on syncing

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("#user-selected").click(function() {
+        $(this).alert("yo");
+        console.log("yes");
+    });
+});
---
+++ js/profile.js
@@ -0,0 +1,12 @@
+// "Edit Profile" button to only appear if the user logged in is viewing their own profile
+$(document).ready(function() {
+    $()
+});
+
+// "Show More Posts" button to show more posts
+$(document).ready(function() {
+    $(".show-more-posts-button").click(function() {
+        // idk yet
+    })
+});
+

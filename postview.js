0af037fee082b2409f32749bebff5a1fa43b5552
rustynade1 <93399085+rustynade1@users.noreply.github.com>
2023-06-23 00:25:19 +0800

    added working upvote/downvote

--- postview.js
+++ postview.js
@@ -1,3 +1,5 @@
+/*********************************************************************/
+//comment related functions
 $(document).ready(function() {
     $("#reply1").click(function() {
       $("#comment-popup1").toggle();
@@ -17,10 +19,27 @@ $(document).ready(function() {
     });
 });
 
+
+
+/*********************************************************************/
+//upvoted related functions
 $(document).ready(function() {
     $("#upvote1").click(function() {
       var counterValue = parseInt($("#vote-value1").text());
-      var newCounterValue = counterValue + 1;
+      var newCounterValue = counterValue;
+      
+      if ($(this).hasClass('clicked')) {
+        newCounterValue--;
+        $('#upvote1').removeClass('clicked');
+      }else if ($('#downvote1').hasClass('clicked')){
+        newCounterValue = counterValue + 2;
+        $(this).addClass('clicked');
+        $('#downvote1').removeClass('clicked');
+      }else{
+        newCounterValue++;
+        $(this).addClass('clicked');
+      }
+      
       $("#vote-value1").text(newCounterValue);
     });
   });
@@ -28,7 +47,20 @@ $(document).ready(function() {
 $(document).ready(function() {
     $("#upvote2").click(function() {
       var counterValue = parseInt($("#vote-value2").text());
-      var newCounterValue = counterValue + 1;
+      var newCounterValue = counterValue;
+      
+      if ($(this).hasClass('clicked')) {
+        newCounterValue--;
+        $('#upvote2').removeClass('clicked');
+      }else if ($('#downvote2').hasClass('clicked')){
+        newCounterValue = counterValue + 2;
+        $(this).addClass('clicked');
+        $('#downvote2').removeClass('clicked');
+      }else{
+        newCounterValue++;
+        $(this).addClass('clicked');
+      }
+
       $("#vote-value2").text(newCounterValue);
     });
   });
@@ -36,18 +68,43 @@ $(document).ready(function() {
 $(document).ready(function() {
     $("#upvote3").click(function() {
       var counterValue = parseInt($("#vote-value3").text());
-      var newCounterValue = counterValue + 1;
+      var newCounterValue = counterValue;
+      
+      if ($(this).hasClass('clicked')) {
+        newCounterValue--;
+        $('#upvote3').removeClass('clicked');
+      }else if ($('#downvote3').hasClass('clicked')){
+        newCounterValue = counterValue + 2;
+        $(this).addClass('clicked');
+        $('#downvote3').removeClass('clicked');
+      }else{
+        newCounterValue++;
+        $(this).addClass('clicked');
+      }
+
       $("#vote-value3").text(newCounterValue);
     });
   });
 
 /*********************************************************************/
-
+//downvote related functions
 
 $(document).ready(function() {
     $("#downvote1").click(function() {
       var counterValue = parseInt($("#vote-value1").text());
-      var newCounterValue = counterValue - 1;
+      var newCounterValue = counterValue;
+      
+    if ($(this).hasClass('clicked')) {
+        newCounterValue++;
+        $('#downvote1').removeClass('clicked');
+    }else if ($('#upvote1').hasClass('clicked')){
+        newCounterValue = counterValue - 2;
+        $(this).addClass('clicked');
+        $('#upvote1').removeClass('clicked');
+    }else{
+        newCounterValue--;
+        $(this).addClass('clicked');
+    }
       $("#vote-value1").text(newCounterValue);
     });
   });
@@ -55,7 +112,19 @@ $(document).ready(function() {
 $(document).ready(function() {
     $("#downvote2").click(function() {
       var counterValue = parseInt($("#vote-value2").text());
-      var newCounterValue = counterValue - 1;
+      var newCounterValue = counterValue;
+      
+    if ($(this).hasClass('clicked')) {
+        newCounterValue++;
+        $('#downvote2').removeClass('clicked');
+    }else if ($('#upvote2').hasClass('clicked')){
+        newCounterValue = counterValue - 2;
+        $(this).addClass('clicked');
+        $('#upvote2').removeClass('clicked');
+    }else{
+        newCounterValue--;
+        $(this).addClass('clicked');
+    }
       $("#vote-value2").text(newCounterValue);
     });
   });
@@ -63,7 +132,21 @@ $(document).ready(function() {
 $(document).ready(function() {
     $("#downvote3").click(function() {
       var counterValue = parseInt($("#vote-value3").text());
-      var newCounterValue = counterValue - 1;
+      var newCounterValue = counterValue;
+
+    if ($(this).hasClass('clicked')) {
+        newCounterValue++;
+        $('#downvote3').removeClass('clicked');
+    }else if ($('#upvote3').hasClass('clicked')){
+        newCounterValue = counterValue - 2;
+        $(this).addClass('clicked');
+        $('#upvote3').removeClass('clicked');
+    }else{
+        newCounterValue--;
+        $(this).addClass('clicked');
+    }
       $("#vote-value3").text(newCounterValue);
     });
   });
+
+/*********************************************************************/

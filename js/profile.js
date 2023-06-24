a1b7becc7694b9cee663b3bb50ffafdad35dd573
wonchuz <120187423+wonchuz@users.noreply.github.com>
2023-06-25 00:22:40 +0800

    implements js funcs in homepage + profile
    display all posts, post, view current user profile, view latest comments/posts

--- js/profile.js
+++ js/profile.js
@@ -1,42 +1,106 @@
-// Display user's latest posts
+let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+let currentPosts = JSON.parse(localStorage.getItem("currentPosts"));
+
+console.log(currentPosts);
+
 $(document).ready(function() {
-    $(".latestposts a").click(function() {
-        if(currentUser === postAuthor) {
-            $(".post").show();
-        }
-    })
-});
+    $("#profile-username").text(currentUser.username);
+    $("#profile-img").attr("src", currentUser.img);
+    $("#profile-bio").text(currentUser.bio);
 
-// Display user's latest comments
-$(document).ready(function() {
-    $(".latestcomments a").click(function() {
-        if(currentUser === postAuthor) {
-            $(".comment").show();
-        }*/
-    })
-});
-
-/*
-const listOfPosts = [];
-document.getElementsByClassName("post").innerHTML = listOfPosts;
-// OR?? idk which one works
-const listOfPosts = Array.from(document.getElementsByClassName(".post"));
-*/
-
-// "Show More Posts" button to show more posts
-function showMorePosts() {
-    $(document).ready(function() {
-        $(".show-more-posts-button").click(function() {
-            $().toggle();
-        })
+    $("#latest-comments").click(function() {
+        console.log("Viewing latest comments");
+        let feedContainer = document.querySelector("#latest-feed");
+        $(this).addClass("active");
+        $("#latest-posts").removeClass("active");
+        feedContainer.innerHTML = "";
+        // displayAllComments();
     });
-}
 
-listOfPosts.forEach(checkUsersPostsAndComments());
+    $("#latest-posts").click(function() {
+        console.log("Viewing latest posts");
+        let feedContainer = document.querySelector("#latest-feed");
 
-function checkUsersPostsAndComments(element) {
+        $(this).addClass("active");
+        $("#latest-comments").removeClass("active");
 
-    if(currentUser === postAuthor) {
-        $(".comment").show();
+        feedContainer.innerHTML = "";
+        displayAllPosts(currentPosts);
+    });
+});
+
+function displayAllPosts(posts) {
+    let reversedposts = posts;
+    reversedposts.reverse();
+
+    for(let post of posts) {
+        console.log("User's post: " + post.user.username);
+        if(post.user.username == currentUser.username) {
+            writePost(post.user, post);
+        }
     }
 }
+
+// TODO: displayAllComments()
+
+console.log(currentPosts);
+
+function writePost(user, post) {
+    const postContainer = document.querySelector("#latest-feed");
+    const item =
+            `<div class="flex-row-container post">
+                <div>
+                    <img class="user" src="${user.img}">
+                </div>
+
+                <div class="flex-column-container post-details">
+                    <p class="username"> ${user.username} </p>
+                    <p class="post-title"> ${post.title} </p>
+                    <p class="description"> ${post.description} </p>
+                    
+                    <div class="actions">
+                        <span class="comment"></span>
+                        <span class="upvote"></span>
+                        <span class="downvote"></span>
+                    </div>
+                </div>
+            </div>`;
+
+    postContainer.innerHTML += item;
+}
+
+// TODO: writeComment()
+
+displayAllPosts(currentPosts);
+
+displayPosts = currentPosts.slice(0, 2);
+
+// "Show More Posts" button
+let showMorePostsBtn = document.querySelector('#show-more-posts-button');
+let numOfPosts = 3;
+
+showMorePostsBtn.onclick = () => {
+    let listOfPosts = [...document.querySelectorAll('.flex-column-container .latest .buttons .flex-row-container')];
+
+    for(let i = numOfPosts; i < numOfPosts + 3; i++) {
+        listOfPosts[i].style.display = 'inline-block';
+    }
+
+    numOfPosts += 3;
+
+    if(numOfPosts >= listOfPosts.length) {
+        showMorePostsBtn.style.display = "none";
+    }
+};
+
+/*
+    they hide the posts using
+    
+    .post:nth-child(3) {
+        display: none;
+    }
+
+    but idk how to make it work
+ */
+
+// "Show More Comments" button
--- js/script.js
+++ js/script.js
@@ -5,9 +5,8 @@ const User = function(name, password) {
     this.password = password;
     this.acc = "#user-" + name;
     this.img = "./profilepics/" + this.lname + ".jpg";
-    this.bio = "";
+    this.bio = "test bio";
 }
-
 
 let userGuest = new User("Guest", "1234");
 let currentUser = userGuest;
@@ -16,32 +15,20 @@ let userChaewon = new User("Chaewon", "tyforsupportingus");
 let userYunjin = new User("Yunjin", "IGOTACONDOINMANHATTAN");
 let userKazuha = new User("Kazuha", "bang!");
 let userEunchae = new User("eunchae", "mubankpresident");
+let posts = [];
 
-/*
-FUNCTIONS TO DO:
-- show edit button
-- view profile
-- create post
-- create comment
-- view post
-- upvote downvote post comment
-- search
-- view profile
--view all posts 
-- view latest comments and posts
-- login logout
-*/
-
-const Post = function(num, title, description, user, comments = []) {
+// Post and Comment Construtors 
+const Post = function(num, user, title, description) {
     this.num = num;
-    this.title = num.toString() + ". " + title;
+    this.title = title;
     this.descsnippet = description.substr(0, 20);
     this.user = user;
     this.description = description;
     this.upvotes = 0;
     this.downvotes = 0;
-    this.comments = comments;
+    this.comments = [];
 }
+
 
 const Comment = function(user, description, comments = []) {
     this.user = user;
@@ -51,26 +38,56 @@ const Comment = function(user, description, comments = []) {
     this.comments = comments;
 }
 
+
+// Put 5 Sample Posts on Website
+let post1 = new Post(1, userSakura, "test title", "test description");
+let post2 = new Post(2, userEunchae, "big announcement", "Ever since Wonyoung's contract for Music Bank expired, they opened auditions! GUESS WHO'S THE NEW MUSIC BANK PRESIDENT??????");
+let post3 = new Post(3, userYunjin, "life update", "BTS PLEASE COME BACK");
+let post4 = new Post(4, userChaewon, "What is Hyewon up to?", "Hyewon still updates her Youtube but not as regularly as before, she hasn't been posting a lot on Instagram like before too sadly.");
+let post5 = new Post(5, userKazuha, "How good was Park Sunghoon as a skater?", "He wasn't Korea's top man for sure haha but is there someone who maybe followed korean skating more or something who could tell me, from objective fs point, how good he actually was? Could he potentially get really on top if he continued? Was there still room for improvement (considering he's 19 rn, so not that young but also not that old) or he probably reached his limits?");
+
+// Put Posts in Arrays
+posts.push(post1);
+posts.push(post2);
+posts.push(post3);
+posts.push(post4);
+posts.push(post5);
+
+localStorage.setItem("currentPosts", JSON.stringify(posts));
+console.log(localStorage);
+
+// Display All Posts from All Users
+function displayAllPosts(posts) {
+    for(let post of posts) {
+        writePost(post.user, post);
+    }
+}
+
+displayAllPosts(posts);
+
+// Functions
+
+// Switch User: updates currentUser
 function switchUser(newUser) {
     $(document).ready(function() {
         $("#current-username").text(newUser.username);
         $("#user-selected").attr("src", newUser.img);
         currentUser = newUser;
-        localStorage.clear();
         localStorage.setItem("currentUser", JSON.stringify(currentUser));
         console.log(localStorage);
     });
 }
 
-function viewingProfile(user) {
-    $(document).load(function() {
-        console.log("Current User Profile: " + user.name);
-        $("#profile-img").attr("src", user.img);
-        $("#profile-username").text("hello");
-        $("#profile-bio").text("it worked");
-    });
+// TODO: View User Profile
+function viewUserProfile(user) {
+    if(user == userGuest) {
+        window.location = "login.html";
+    } else {
+        window.location = "profile.html";
+    }
 }
 
+// Hides Edit Profile Button
 function hideEditProfile(user) {
     const editProfileButton = document.getElementsByName("Edit Profile");
 
@@ -87,21 +104,78 @@ function hideEditProfile(user) {
     });
 }
 
+// Post
+
+function getInputs(user) {
+    if(currentUser == userGuest) {
+        window.location = "login.html";
+    } else {
+        let formElement = document.forms.postform;
+        let formData = new FormData(formElement);
+        let count = 0;
+        let title = "";
+        let description = "";
+
+        for (let data of formData) {
+            if(count == 0) {
+                title = data[1];
+            } else {
+                description = data[1];
+            }
+
+            count += 1;
+        }
+
+        let post = new Post(posts.length + 1, user, title, description);
+        posts.push(post);
+        localStorage.setItem("currentPosts", JSON.stringify(posts));
+        writePost(currentUser, post);
+    }
+}
+
+function writePost(user, post) {
+    const postContainer = document.querySelector("#posts-feed");
+    const item =
+            `<div class="flex-row-container post">
+                <div>
+                    <img class="user" src="${user.img}">
+                </div>
+
+                <div class="flex-column-container post-details">
+                    <p class="username"> ${user.username} </p>
+                    <p class="post-title"> ${post.title} </p>
+                    <p class="description"> ${post.description} </p>
+                        
+                    <div class="actions">
+                        <span class="comment"></span>
+                        <span class="upvote"></span>
+                        <span class="downvote"></span>
+                    </div>
+                </div>
+            </div>`;
+
+    postContainer.innerHTML += item;
+}
+
 // Switching User on Home Page
 $(document).ready(function() {
-    // View Current User's Profile
-    $("#user-selected").click(function() {
+
+    $("#submit-post").click(function() {
+        console.log("Posting...");
         if(currentUser == userGuest) {
             window.location = "login.html";
         } else {
-            window.location = "profile.html";
-            console.log(localStorage);
-            currentUser = JSON.parse(localStorage.getItem('currentUser'));
-            console.log("Viewing Current User's Profile: " +  currentUser.name);
-            viewingProfile(currentUser);
+            getInputs(currentUser);
         }
+    })
+
+    // View Current User's Profile
+    $("#user-selected").click(function() {
+        viewUserProfile(currentUser);
     });
 
+
+    // Switches User to clicked icon
     $("#user-guest").click(function() {
         let user = userGuest;
         console.log(user.name);

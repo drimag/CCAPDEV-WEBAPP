55532f8764caa91af5b1b80a58a55ca095cd52b7
rustynade1 <93399085+rustynade1@users.noreply.github.com>
2023-07-22 16:16:24 +0800

    voting through a post view affects db, separated voting from viewpost.js

---
+++ public/js/voting.js
@@ -0,0 +1,132 @@
+$(document).ready(function(){
+    // Upvote when viewing a post
+    $("button.upvote").click(async function() {
+
+        //var made to find this upvote's corresponding downvote button
+        let downvoteButton = $(this).nextAll("button.downvote").first();
+
+        let postNumber = $(this).closest(".post").attr("id").slice(-1); 
+        
+        let voteCount = $(this).siblings(".number"); 
+        let currentVotes = parseInt(voteCount.text()); 
+
+        // Removes vote if clicked again
+		if ($(this).hasClass('clicked')) {
+			//decrease vote count here by 1
+            newVotes = currentVotes - 1;
+            voteCount.text(newVotes);
+
+			$(this).removeClass('clicked');
+        }
+		// Removes effect of opposite vote if clicked
+		else if (downvoteButton.hasClass('clicked')){
+			//increase the vote count by 2,because it nullifies the downvote
+            newVotes = currentVotes + 2;
+            voteCount.text(newVotes);
+			$(this).addClass('clicked');
+			downvoteButton.removeClass('clicked');
+        }   
+		//When no vote was clicked
+		else{
+			//increase vote count here by 1
+            newVotes = currentVotes + 1;
+            voteCount.text(newVotes);
+
+			$(this).addClass('clicked');
+		}
+
+
+        const data = {
+            votes: newVotes,
+            postId: postNumber,
+            action: "upvote"
+        };
+        console.log(data);
+
+        const jString = JSON.stringify(data);
+    
+        const response = await fetch("/posts/_id/votes", {
+            method: 'POST',
+            body: jString,
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
+
+
+
+        if(response.status === 200) {
+            //location.reload();
+        } else {
+            console.log("Status code received: " + response.status);
+        }
+
+    });
+    
+
+ 
+    // Downvote when viewing a post
+    $("button.downvote").click(async function() {
+            
+        //var made to find this downvote's corresponding upvote button
+        let upvoteButton = $(this).prevAll("button.upvote").first();
+            
+
+        let postNumber = $(this).closest(".post").attr("id").slice(-1); 
+
+        let voteCount = $(this).siblings(".number"); 
+        let currentVotes = parseInt(voteCount.text()); 
+
+        //removes vote if clicked again  
+		if ($(this).hasClass('clicked')) {
+			//increase vote count here by 1
+            newVotes = currentVotes + 1;
+            voteCount.text(newVotes);
+			$(this).removeClass('clicked');
+        }
+		//removes effect of opposite vote if clicked
+		else if (upvoteButton.hasClass('clicked')){
+			//decrease the vote count by 2,because it nullifies the upvote
+            newVotes = currentVotes - 2;
+            voteCount.text(newVotes);
+			$(this).addClass('clicked');
+			upvoteButton.removeClass('clicked');
+        }
+		//when no vote was clicked
+        else{
+			//decrease vote count here by 1
+            newVotes = currentVotes - 1;
+            voteCount.text(newVotes);
+			$(this).addClass('clicked');
+		}
+
+
+        const data = {
+            votes: newVotes,
+            postId: postNumber,
+            action: "downvote"
+        };
+        console.log(data);
+    
+        const jString = JSON.stringify(data);
+        
+        const response = await fetch("/posts/_id/votes", {
+            method: 'POST',
+            body: jString,
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
+    
+    
+    
+        if(response.status === 200) {
+            //location.reload();
+        } else {
+            console.log("Status code received: " + response.status);
+        }
+
+    });
+     
+
+});

1616daf9f87b6fc423581d2840fc28cf809f7e1b
wonchuz <jaeme_patrice_rebano@dlsu.edu.ph>
2023-07-23 23:36:05 +0800

    Update edit.js

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,5 +1,3 @@
-const id = 0;
-
 $(document).ready(function() {
     $(".edit-comment").click(function() {
         $(this).hide();
@@ -20,3 +18,65 @@ $(document).ready(function() {
     });
     // cancel edit comment ?
 });
+
+/*
+const submitBtn = document.querySelector("#editComment");
+const editCommentForm = document.forms.editCommentForm;
+
+submitBtn?.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const formData = new FormData(editCommentForm);
+    console.log("Submit Edit Comment Data");
+
+    id = comment_id;
+    console.log(id);
+
+    const currentUser =$("#currentUser-navuser").text();
+    const new_comment = formData.get("new-comment");
+
+    let data = {
+        id: postID,
+        comment: new_comment
+    };
+    
+    console.log(data);
+    const jString = JSON.stringify(data);
+    console.log(jString);
+    
+    try {
+        let response = await fetch("/comment?loggedIn=" + currentUser, {
+            method: 'PUT',
+            body: jString,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        console.log(response);
+
+        if(response.status === 200) {
+            location.reload();
+        } else {
+            console.log("Status code received: " + response.status);
+        }
+
+        // update post
+        response = await fetch("/post/addedcomment?loggedIn=" + currentUser, {
+            method: 'PUT',
+            body: jString,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if(response.status === 200) {
+            location.reload();
+        } else {
+            console.log("Status code received: " + response.status);
+        }
+        
+    } catch (err) {
+        console.error(err);
+    }
+});
+*/

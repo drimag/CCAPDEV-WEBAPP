6a6ae8e2993997f93c498244dc26fee2fb6c81bc
drimag <106684744+drimag@users.noreply.github.com>
2023-07-24 18:57:01 +0800

    Add files via upload

--- src/controllers/voteController.js
+++ src/controllers/voteController.js
@@ -22,27 +22,27 @@ const voteController = {
         const { votes } = req.body;
         console.log("votes are:",votes);
         try {
-         
-          // Find the post by its ID
-          //const post = await posts.findOne({ num: parseInt(postId)});
-          //checks if id belongs to a comment or post
-          //let element = id.slice(0,-1);
-          if (type === "comment"){
-            console.log("This is a comment");
-            const comment = await comments.findOne({ num: parseInt(number)});
-            comment.votes = votes;
-            await comments.updateOne({num: parseInt(number) }, { $set: { votes } });
-          }else{
-            console.log("This is a post");
-            const post = await posts.findOne({ num: parseInt(number)});
+			
+			// Find the post by its ID
+			//const post = await posts.findOne({ num: parseInt(postId)});
+			//checks if id belongs to a comment or post
+			//let element = id.slice(0,-1);
+			if (type === "comment"){
+				console.log("This is a comment");
+				const comment = await comments.findOne({ num: parseInt(number)});
+				comment.votes = votes;
+				await comments.updateOne({num: parseInt(number) }, { $set: { votes } });
+			}else{
+				console.log("This is a post");
+				const post = await posts.findOne({ num: parseInt(number)});
 
-            // Update the vote count in the post document
-            post.votes = votes;
+				// Update the vote count in the post document
+				post.votes = votes;
 
-            // Save the updated post back to the database
-            await posts.updateOne({num: parseInt(number) }, { $set: { votes } });
-            
-          }
+				// Save the updated post back to the database
+				await posts.updateOne({num: parseInt(number) }, { $set: { votes } });
+				
+			}
 
           
       
@@ -50,15 +50,42 @@ const voteController = {
           //req.body.postId
         
       
-          res.status(200).json({ message: 'Vote updated successfully' });
+       	   	res.status(200).json({ message: 'Vote updated successfully' });
     
     
         } catch (error) {
-          console.error('Failed to update vote:', error);
+        	console.error('Failed to update vote:', error);
           
-          res.status(500).json({ message: 'Failed to update vote' });
+        	res.status(500).json({ message: 'Failed to update vote' });
         }
-      }
+    },
+
+    getUserVotes: async function (req, res) {
+		console.log("user vote getter called");
+
+		let curr = req.query.loggedIn;
+		console.log(curr);
+		const user = await users.findOne({username: curr});
+
+		const votes = { 
+			upvoteComments: user.upvoteComments,  
+			downvoteComments: user.downvoteComments,
+			upvotePosts: user.upvotePosts,
+			downvotePosts: user.downvotePosts
+		}
+		
+		console.log("sending this as user's votes': " + votes);
+
+		try {
+
+            console.log("sending this as user's votes': " + votes);
+            res.send(votes);
+
+        } catch (error) {
+            console.error(error);
+            res.sendStatus(500); // fix
+        }
+	}
 }
 
 export default voteController;

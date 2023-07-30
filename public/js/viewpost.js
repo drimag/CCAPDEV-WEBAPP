b50683cf8e72b4d3c6a102b9b2c502d94823f73f
wonchuz <jaeme_patrice_rebano@dlsu.edu.ph>
2023-07-31 06:39:23 +0800

    nested comment reply pop up fixed

--- public/js/viewpost.js
+++ public/js/viewpost.js
@@ -15,11 +15,11 @@ $(document).ready(function() {
         $(this).css('cursor', 'pointer');
     });
 
-    // Comment on a Comment
+    // Comment on a Comment (fixed)
     $("button.comment").click(function() {
         const comment_id = $(this).attr('id');
         console.log(comment_id);
-        const reply_pop = $("div#comment" + comment_id + " div.reply-pop");
+        const reply_pop = $("div#comment" + comment_id + " div.reply-pop" + comment_id);
         const visible = reply_pop.is(":visible");
         if(visible) {
             reply_pop.hide();
